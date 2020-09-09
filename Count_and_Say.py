f8c9a28df74009c36652f5ef250745cedfd699fc
bamboosteam <t.t.yeah.b5@gmail.com>
2020-09-09 23:03:09 +0900

    Solvedgit add Count_and_Say.py

--- Count_and_Say.py
+++ Count_and_Say.py
@@ -1,11 +1,13 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
         MAX = 30
-        sequences = {}
+        sequences = {1:'1'}
         digit = '1'
-        sequences[1] = digit
+        # Create the mountain of digits for count-n-say
         for i in range(MAX - 1):
+            # Initiate value for count-n-say
             value = ''
+            # Count-n-say the previous line
             while digit:
                 temp = digit[0]
                 res = 0
@@ -16,6 +18,8 @@ class Solution:
                 digit = digit.lstrip(digit[0])
                 value += str(res)
                 value += temp
+            # Update digit for next pile
+            digit = value
+            # Register the value and key as dictionary
             sequences[i + 2] = value
-
         return sequences[n]
