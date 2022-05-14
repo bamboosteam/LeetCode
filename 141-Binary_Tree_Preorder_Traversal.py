21604dffe08c77fc953f28be7f7c0179135dbc17
Takuma Takezawa <takumatakezawa@Takumas-MacBook-Air.local>
2022-05-15 01:13:31 +0900

    added solution

--- 141-Binary_Tree_Preorder_Traversal.py
+++ 141-Binary_Tree_Preorder_Traversal.py
@@ -25,3 +25,23 @@ class Solution:
                 curr = right.pop()
                 
         return result
+
+
+# Solution 1
+# class Solution(object):
+#     def preorderTraversal(self, root):
+#         if root is None:
+#             return []
+
+#         stack, output = [root, ], []
+
+#         while stack:
+#             root = stack.pop()
+#             if root is not None:
+#                 ourput.append(root.val)
+#                 if root.right is not None:
+#                     stack.append(root.right)
+#                 if root.left is not None:
+#                     stack.append(root.left)
+
+#         return output
