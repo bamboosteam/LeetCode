8985b98e7d1506517c802846bceabb9ea7fd31b1
Takuma Takezawa <takumatakezawa@Takumas-MacBook-Air.local>
2022-06-21 23:19:31 +0900

    solved using stack

--- 150-Evaluate_Reverse_Polish_Notation.py
+++ 150-Evaluate_Reverse_Polish_Notation.py
@@ -1,21 +1,22 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        ope = ["+", "-","/","*"]
-        
-        idx = 0
-        
-        while len(tokens) > 1:
-            if tokens[idx] in ope:
-                if tokens[idx] == "+":
-                    tokens[idx-2] = str(int(tokens[idx-2]) + int(tokens[idx-1]))
-                elif tokens[idx] == "-":
-                    tokens[idx-2] = str(int(tokens[idx-2]) - int(tokens[idx-1]))
-                elif tokens[idx] == "/":
-                    tokens[idx-2] = str(int(int(tokens[idx-2]) / int(tokens[idx-1])))
-                else:
-                    tokens[idx-2] = str(int(tokens[idx-2]) * int(tokens[idx-1]))
-                del tokens[idx-1:idx+1]
-                idx -= 2
-            idx += 1
-        
-        return tokens[0]
+        stack = []
+        for i in tokens:
+            if i == "+":
+                x, y = stack.pop(), stack.pop()
+                stack.append(x + y)
+                continue
+            elif i == "-":
+                x, y = stack.pop(), stack.pop()
+                stack.append(y - x)
+                continue
+            elif i == "*":
+                x, y = stack.pop(), stack.pop()
+                stack.append(x * y)
+                continue
+            elif i == "/":
+                x, y = stack.pop(), stack.pop()
+                stack.append(int(y/x))
+                continue
+            stack.append(int(i))
+        return stack[0]
